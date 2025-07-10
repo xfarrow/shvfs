ba17073d3650313c0dc847a8084693a0ae97254b
xfarrow <49845537+xfarrow@users.noreply.github.com>
2025-07-10 21:20:44 +0200

    Update shvfs.c

--- shvfs.c
+++ shvfs.c
@@ -10,10 +10,25 @@
 #define APPEND_TEXT "checksum_here"
 
 static char *file_data = NULL;
-static size_t file_size = 0;
+static size_t file_size = 1024;
 
+// Get file attributes (metadata).
+// path: the path the kernel is requesting info about 
+// stbuf: struct to fill
+// fi: extra info
 static int myfs_getattr(const char *path, struct stat *stbuf, struct fuse_file_info *fi) {
-    memset(stbuf, 0, sizeof(struct stat));
+    memset(stbuf, 0, sizeof(struct stat)); // Set all bytes to zero so we're sure we don't return junk
+
+    /*
+        https://www.man7.org/linux/man-pages/man7/inode.7.html
+        S_IFSOCK   0140000   socket
+        S_IFLNK    0120000   symbolic link
+        S_IFREG    0100000   regular file
+        S_IFBLK    0060000   block device
+        S_IFDIR    0040000   directory
+        S_IFCHR    0020000   character device
+        S_IFIFO    0010000   FIFO
+    */
 
     if (strcmp(path, "/") == 0) {
         stbuf->st_mode = S_IFDIR | 0755;
@@ -25,12 +40,10 @@ static int myfs_getattr(const char *path, struct stat *stbuf, struct fuse_file_i
     } else {
         return -ENOENT;
     }
-
     return 0;
 }
 
-static int myfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                        off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags) {
+static int myfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags) {
     if (strcmp(path, "/") != 0)
         return -ENOENT;
 
